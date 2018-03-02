bf45166c5cbac669f29e68261f6dc28ee533f4aa
Emmanuel Chêne <emma.chene@gmail.com>
2018-03-02 16:41:37 +0100

    Fin TP03

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,14 +1,22 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.*;
 
 public class App {
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		String titre = ResourceBundle.getBundle("application").getString("titre");
 
-		String ascciiArt = FigletFont.convertOneLine("HELLO WORLD");
+		String ascciiArt = FigletFont.convertOneLine(titre);
 
 		System.out.println(ascciiArt);
+		
+		// Affichage de l'environnment
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		
+		System.out.println("Environnement : " + environnement);
+		
 	}
 }
